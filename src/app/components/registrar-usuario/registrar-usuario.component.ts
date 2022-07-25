6889de7238b19bd3fbd6e516b9cb4d2c059efa11
SanchezEmir <emir2104@gmail.com>
2022-07-25 18:43:09 -0500

    registrar usuario form reactivo

--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarUsuarioComponent implements OnInit {
 
-  constructor() { }
+  registrarUsuario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registrarUsuario = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repetirPassword: ['', [Validators.required, Validators.minLength(6)]],
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  registrar(){
+    const email = this.registrarUsuario.value.email;
+    const password = this.registrarUsuario.value.password;
+    const repetirPassword = this.registrarUsuario.value.repetirPassword;
+    console.log(email, password, repetirPassword);
+    console.log(this.registrarUsuario);
+    console.log(this.registrarUsuario.value);
   }
 
 }
