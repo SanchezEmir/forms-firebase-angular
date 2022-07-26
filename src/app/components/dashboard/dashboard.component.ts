59d4c0e3626c8340b98d149fd04f18f7e0780192
SanchezEmir <emir2104@gmail.com>
2022-07-26 00:24:17 -0500

    dashboad y cerrar sesion

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FirebaseCodeErrorsService } from 'src/app/services/firebase-code-errors.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  dataUser: any;
+
+  constructor(
+    private authService: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    private _serviceFCError: FirebaseCodeErrorsService
+  ) {
+
+  }
 
   ngOnInit(): void {
+    this.authService.currentUser.then(user => {
+      if(user){
+        if(user.emailVerified){
+          this.dataUser = user;
+          this.router.navigate(['/dashboard']);
+        }
+      } else {
+        this.toastr.warning('Por favor, inicia sesión.', 'Alerta');
+        this.router.navigate(['/login']);
+        // this.authService.signOut();
+      }
+    });
+  }
+
+  logOut() {
+    // localStorage.removeItem('token');
+    this.authService.signOut().then(() => {
+      this.router.navigate(['/login']);
+    } ).catch(err => {
+      console.log(err);
+      this.toastr.error(this._serviceFCError.firebaseCodeError(err.code), 'Error');
+    })
   }
 
 }
